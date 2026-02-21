5d2060dd31d4b12396058065290f86033e7517c8
Fakhri Rizaldi <muhamadfakhri06@gmail.com>
2026-02-21 11:48:38 +0700

    Remove name from navbar

--- index.php
+++ index.php
@@ -98,9 +98,7 @@ $data = require 'data.php';
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex items-center justify-between h-16">
                 <div class="flex items-center">
-                    <a href="#" class="text-xl font-bold text-gray-900 dark:text-white tracking-tight">
-                        <?= $data['profile']['name'] ?><span class="text-primary">.</span>
-                    </a>
+                    <!-- Name removed as requested -->
                 </div>
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-8">
